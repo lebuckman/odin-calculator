d7c739bd38699f43a6a65aed31632d8d95816ec1
lebuckman <liambuckman0@gmail.com>
2025-08-22 18:38:50 -0700

    Create basic calculator functions

--- script.js
+++ script.js
@@ -36,3 +36,21 @@ function handleOperator(op) {
     currentNum = "";
     currentNumberDisplay.textContent = "";
 }
+
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+    return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+    if (b === 0) return 0; // Return 0 when dividing by 0
+
+    return Number(a) / Number(b);
+}
